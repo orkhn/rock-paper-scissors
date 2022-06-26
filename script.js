73f3f3c5f1e11bac3650af6e8d639d1394e1b8de
orkhn <orxan.ahadov@gmail.com>
2022-06-26 23:03:46 +0400

    add starter button element

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 // Variables
 // Selectors
 // Functions
+
+//  Function to play main button audio on click
+function playSound() {
+  const btnPress = document.getElementById("click-sound");
+  btnPress.play();
+}
+
+starterBtn = document
+  .getElementById("starter-button")
+  .addEventListener("click", playSound);
